b3d9ecbe810cd06b73bd1307fdef9c3b46eb205a
JirkaDellOro <del@hs-furtwangen.de>
2024-12-10 10:34:37 +0100

    compiled with esnext

--- Agents/Agent00.js
+++ Agents/Agent00.js
@@ -1,11 +1,8 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Agent00 = void 0;
-var Agent00;
+export var Agent00;
 (function (Agent00) {
     function getCommand(_state) {
         let command = { module: "", method: "" };
         return command;
     }
     Agent00.getCommand = getCommand;
-})(Agent00 || (exports.Agent00 = Agent00 = {}));
+})(Agent00 || (Agent00 = {}));
